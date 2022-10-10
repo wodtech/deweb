d8588b130f62005487892376194114dbe5a18fa6
Vladislav Manoly <vladislav.manoly@gmail.com>
2022-10-10 19:35:03 +0300

    responsive, careers updates, add sidebar ex

---
+++ components/common/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-navigation-drawer
+    v-on="$listeners"
+    v-bind="$attrs"
+    :stateless="false"
+    absolute
+    bottom
+    temporary
+  >
+    <v-list
+      
+      nav
+      dense
+    >
+      <v-list-item-group
+        v-model="group"
+        color="black"
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item  >
+          <v-btn  to="for-developers" nuxt rounded class="d-md-flex d-none" text>
+            for developers
+          </v-btn>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    group: null,
+  }),
+  watch: {
+    group () {
+      this.$emit('disableDrawer')
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
